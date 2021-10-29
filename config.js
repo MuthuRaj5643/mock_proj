95f33b41eb4ca8a996fd3304d8fd7aeecd62fcde
MuthuRaj5643 <53951823+MuthuRaj5643@users.noreply.github.com>
2021-10-29 19:51:59 +0530

    config.js

---
+++ config.js
@@ -0,0 +1,10 @@
+window._config = {
+    cognito: {
+        userPoolId: 'us-east-2_IjdDkVzck', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '6lff0opu2k5vn9bq03el81ae87', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-east-2' // e.g. us-east-2
+    },
+    api: {
+        invokeUrl: 'https://main.d266q3jptk5by6.amplifyapp.com/' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+    }
+};
